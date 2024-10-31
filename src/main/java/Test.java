3fc36c89426a4a613802cba1f5c58ad00b7ee852
wehcoder <3037502828@qq.com>
2024-10-31 22:53:31 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 微信公众号：weh程序猿
+ *
+ * @author weh-coder
+ * @des 这是一个类
+ * @date 2024/10/31
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
